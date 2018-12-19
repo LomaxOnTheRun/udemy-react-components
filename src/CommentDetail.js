ee984c80bd8e253a35599274f92cc88716f543ae
LomaxOnTheRun <lomax.on.the.run@gmail.com>
2018-12-19 12:58:54 +1100

    Just pass in props, not deconstructed

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,20 +1,19 @@
 import React from "react";
-import faker from "faker";
 
-const CommentDetail = ({ author, date, text }) => {
+const CommentDetail = props => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
-          {author}
+          {props.author}
         </a>
         <div className="metadata">
-          <span className="date">{date}</span>
+          <span className="date">{props.date}</span>
         </div>
-        <div className="text">{text}</div>
+        <div className="text">{props.content}</div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Ben" date="NOW!" text="Boo ya!" />
-      <CommentDetail author="Gareth" date="Yesterday" text="Duuuude!" />
-      <CommentDetail author="Sara" date="A year ago" text="Oh dear..." />
+      <CommentDetail
+        author="Ben"
+        date="NOW!"
+        content="Boo ya!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Gareth"
+        date="Yesterday"
+        content="Duuuude!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Sara"
+        date="A year ago"
+        content="Oh dear..."
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
