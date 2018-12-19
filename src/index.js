d7883575f0f03eb5454a6ee8c843204c23e4201f
LomaxOnTheRun <lomax.on.the.run@gmail.com>
2018-12-19 13:29:57 +1100

    Add ApprovalCard

--- src/index.js
+++ src/index.js
@@ -3,28 +3,46 @@ import ReactDOM from "react-dom";
 import faker from "faker";
 
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Ben"
-        date="NOW!"
-        content="Boo ya!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Gareth"
-        date="Yesterday"
-        content="Duuuude!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Sara"
-        date="A year ago"
-        content="Oh dear..."
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>This is a simple choice</ApprovalCard>
+
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Ben"
+          date="NOW!"
+          content="Boo ya!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Gareth"
+          date="Yesterday"
+          content="Duuuude!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Sara"
+          date="A year ago"
+          content="Oh dear..."
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
